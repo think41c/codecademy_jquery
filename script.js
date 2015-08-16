7bdfe82cdf1d27cedfd5e1e147656ddf171fd2a3
Tony DiNitto <think41c@gmail.com>
2015-08-15 23:32:01 -0500

    Fading out divs (or any html tag).

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
-   $('div').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
-   $('div').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       }); 
-   });
-   $('div').click(function() {
-       $(this).toggle(1000);
-   }); 
+    $('div').fadeOut(1000);
 });
